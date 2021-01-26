8d4e78ead26d61e85c24446cb58d57bb17071ea8
aditya-singh-07 <adityakusumlata@gmail.com>
2021-01-26 19:50:35 +0530

    issue with file_context not found fixed

--- unpack.bat
+++ unpack.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDpQRRCbPVeeCaIS5Of66/m7t0MNVeE0NYPU3bvAKeMcig==
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRnVphFQ
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJGur2X4FJgtVTw+LMmz3EroT5NTu4fqGoUYYRqw6YIq7
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 cls
 echo.  
@@ -509,7 +542,7 @@ cls
 set /p systemsize=<"bin\tmp\system\system_size.txt"
 echo -   Repack system  
 rem bin\windows\make_ext4fs -L system -T -6 -S bin\tmp\system_file_contexts -C bin\tmp\system_fs_config -l %systemsize%0 -a system  bin\tmp\system.img bin\tmp\system\system  >nul >> log.txt
-bin\windows\make_ext4fs -s -L system -T -6 -S bin\tmp\system\system_file_contexts -C bin\tmp\system\system_fs_config -l %systemsize%0 -a system bin\tmp\system.img bin\tmp\system\system >nul >> log.txt
+bin\windows\make_ext4fs -s -L system -T -6 -S bin\tmp\system_file_contexts -C bin\tmp\system_fs_config -l %systemsize%0 -a system bin\tmp\system.img bin\tmp\system\system >nul >> log.txt
 rem bin\windows\resize2fs -M bin\tmp\system.img >nul 2>nul  >> log.txt
 echo -   Successfully repack system
 echo 1) open system folder
